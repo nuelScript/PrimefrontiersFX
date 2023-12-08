f4dee591898e81a10a9df85d1a6731d2af16cd21
Parseval <igwee3333@gmail.com>
2023-12-08 23:21:35 +0100

    chore: added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToasterProvider } from "@/components/toaster-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           >
             <ToasterProvider />
             {children}
+            <SpeedInsights />
           </ThemeProvider>
         </body>
       </html>
