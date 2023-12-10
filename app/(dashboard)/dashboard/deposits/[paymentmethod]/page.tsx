ed171ab049ab86e2436403b29dbc11812607d0e8
Parseval <igwee3333@gmail.com>
2023-12-10 16:43:56 +0100

    chore: fixed payment page

--- app/(dashboard)/dashboard/deposits/[paymentmethod]/page.tsx
+++ app/(dashboard)/dashboard/deposits/[paymentmethod]/page.tsx
@@ -6,16 +6,18 @@ const Paymentmethod = ({ params }: { params: { paymentmethod: string } }) => {
       <p className="font-semibold text-2xl text-primary">
         Pay via {params.paymentmethod}
       </p>
-      <div className="flex justify-center h-10 w-full text-primary tracking-wide p-10">
-        Please contact{" "}
+      <div className="h-16 w-full text-primary tracking-wide p-10 text-center">
+        <span> Please contact </span>
         <Link
           href="mailto:info@primefrontiersFX.com"
-          className="text-slate-700 mx-2 hover:underline"
+          className="text-slate-700 hover:underline"
         >
           info@primefrontiersFX.com
         </Link>
-        {"  "}
-        to make payment via {params.paymentmethod}
+        <span>
+          {"  "}
+          to make payment via {params.paymentmethod}
+        </span>
       </div>
     </div>
   );
